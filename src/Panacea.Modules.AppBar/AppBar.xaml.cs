bbac86b635b562f1e090bab8a9c8871d35c457fd
Portno <j.remoundos@dotbydot.gr>
2019-07-01 17:35:02 +0300

    first commit

---
+++ src/Panacea.Modules.AppBar/AppBar.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Panacea.Modularity.AppBar;
+using Panacea.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Native.Extensions;
+using System.Windows.Shapes;
+
+namespace Panacea.Modules.AppBar
+{
+    /// <summary>
+    /// Interaction logic for AppBar.xaml
+    /// </summary>
+    public partial class AppBar : Window, IAppBar
+    {
+        public AppBar()
+        {
+            InitializeComponent();
+        }
+
+        public void AddButton(ViewModelBase vm)
+        {
+
+        }
+
+        void IAppBar.Show()
+        {
+            this.ToAppBar(System.Windows.Native.ABEdge.Bottom);
+            Show();
+        }
+
+        void IAppBar.Hide()
+        {
+            Hide();
+        }
+
+        public void RemoveButton(ViewModelBase vm)
+        {
+
+        }
+    }
+}
---
+++ src/Panacea.Modules.AppBar/AppBarPlugin.cs
@@ -0,0 +1,38 @@
+﻿using Panacea.Modularity.AppBar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panacea.Modules.AppBar
+{
+    public class AppBarPlugin : IAppBarPlugin
+    {
+        public Task BeginInit()
+        {
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public Task EndInit()
+        {
+            return Task.CompletedTask;
+        }
+
+        IAppBar _appbar;
+        public IAppBar GetAppBar()
+        {
+            return _appbar ?? (_appbar = new AppBar());
+        }
+
+        public Task Shutdown()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
